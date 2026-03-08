54022a0ccfa6fede46d4079108e9a33702e763dd
Rowan Sarty <200936268+Rowan0404@users.noreply.github.com>
2026-03-08 14:14:59 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Strength-Quest/sw.js', { scope: '/Strength-Quest/' })})}
